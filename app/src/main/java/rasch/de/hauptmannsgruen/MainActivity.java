1bbeeaab39ab59f98a0f7af8491e3e47fa9d85df
Mr.Hmg <rasch@myhmg.de>
2017-02-11 18:23:57 +0100

    first

---
+++ app/src/main/java/rasch/de/hauptmannsgruen/MainActivity.java
@@ -0,0 +1,54 @@
+package rasch.de.hauptmannsgruen;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        webView =(WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        String summary = "<html><body>Loading ...</body></html>";
+        webView.loadData(summary, "text/html", null);
+
+        webView.loadUrl("http://rasch.myhmg.de/hmgFeed.php");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
