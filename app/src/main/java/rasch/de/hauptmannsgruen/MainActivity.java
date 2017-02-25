85d7bc55acd9905ac68758e4b90001e8cc386c58
Mr.Hmg <rasch@myhmg.de>
2017-02-25 13:51:13 +0100

    Add Event

--- app/src/main/java/rasch/de/hauptmannsgruen/MainActivity.java
+++ app/src/main/java/rasch/de/hauptmannsgruen/MainActivity.java
@@ -1,37 +1,69 @@
 package rasch.de.hauptmannsgruen;
 
-import android.app.Dialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.WebView;
-import android.widget.Button;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.app.Dialog;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView webView;
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        webView =(WebView) findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        String summary = "<html><body>Loading ...</body></html>";
-        webView.loadData(summary, "text/html", null);
-
-        webView.loadUrl("http://rasch.myhmg.de/myHmgFeed/hmgFeed.php");
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        /*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
 
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,18 +77,12 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        //if (id == R.id.action_settings) {
-        //    return true;
-        //}
         switch (item.getItemId()) {
             case R.id.action_settings:
 				/*
 				Intent intent = new Intent(MainActivity.this, WebActivity.class);
 				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				intent.putExtra("LatValue", (int)39485000); 
+				intent.putExtra("LatValue", (int)39485000);
 				intent.putExtra("LongValue", (int)-80142777);
 				startActivity(intent);
 				return true;
@@ -70,34 +96,112 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-	
-	/**
-	 * 
-	*/
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+
+            switch(position) {
+                case 0:
+                    return NewsFragment.newInstance("","");
+                case 1:
+                    return EventFragment.newInstance("","");
+
+            }
+
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "News";
+                case 1:
+                    return "Events";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
+    }
+
+
+
     private void doDialogAbout() {
-    	
-    	try{
-    		
-    		final Dialog dialog = new Dialog(this);
-    		dialog.setContentView(R.layout.about_dlg);
-    		dialog.setTitle("Info..." );
-   
-	 		Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
-	 		//TextView dialogText = (TextView) dialog.findViewById(R.id.textView1);
-	 		
-	 		dialogButton.setOnClickListener(new View.OnClickListener() {
-	  				@Override
-	  				public void onClick(View v) {
-	  					dialog.dismiss();
-	  				}
-	  		});
-	  		dialog.show();
-	  		
-	    }catch(Exception e) {
-	    }
-	}
-	
-	
-	
-	
+
+        try{
+
+            final Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.about_dlg);
+            dialog.setTitle("Info..." );
+
+            Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+            //TextView dialogText = (TextView) dialog.findViewById(R.id.textView1);
+
+            dialogButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+            dialog.show();
+
+        }catch(Exception e) {
+        }
+    }
+
+
 }
